7426570763963f50d67ca41b7c8c3050130eedde
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2016-11-08 21:44:09 +0100

    ios/libwally-core-ios: initial commit

---
+++ ios/libwally-core-ios/libwally-core-ios/libwally_core_ios.h
@@ -0,0 +1,13 @@
+//
+//  libwally_core_ios.h
+//  libwally-core-ios
+//
+//  Created by isidoro carlo ghezzi on 11/8/16.
+//  Copyright © 2016 isidoro carlo ghezzi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libwally_core_ios : NSObject
+
+@end
---
+++ ios/libwally-core-ios/libwally-core-ios/libwally_core_ios.m
@@ -0,0 +1,13 @@
+//
+//  libwally_core_ios.m
+//  libwally-core-ios
+//
+//  Created by isidoro carlo ghezzi on 11/8/16.
+//  Copyright © 2016 isidoro carlo ghezzi. All rights reserved.
+//
+
+#import "libwally_core_ios.h"
+
+@implementation libwally_core_ios
+
+@end
